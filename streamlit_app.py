dce92c2a17f628e3d136d1b894e1ab89ea0244ae
Benno Weck <13520622+Bomme@users.noreply.github.com>
2022-01-28 12:29:02 +0100

    also get query parameters from a separate script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-if "some_string" not in st.session_state:
-  params = st.experimental_get_query_params()
+import separate_script
 
+params = st.experimental_get_query_params()
+params_separate = separate_script.get_query_params()
+
+st.write("Main script")
 st.write(params)
+
+st.write("separate script")
+st.write(params_separate)
