aeec949d297e77708feb63fa2a86f66bbeba63f8
Benno Weck <13520622+Bomme@users.noreply.github.com>
2022-01-31 17:44:06 +0100

    Write something to session_state if params empty

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,14 @@ import separate_script
 
 params = st.experimental_get_query_params()
 logging.info(f"{params=}")
+if not params:
+  st.session_state["MY_FIELD"] = "some value"
+
 params_separate = separate_script.get_query_params()
-logging.info(f"{params_separate=}")
+if "MY_FIELD" in st.session_state:
+  logging.info(f"{params_separate=} {st.session_state["MY_FIELD"]=}")
+else:
+  logging.info(f"{params_separate=}")
 
 st.write("Main script")
 st.write(params)
