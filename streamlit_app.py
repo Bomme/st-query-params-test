883516bcabfcd82407f9b1e951fc3101c4ee3053
Benno Weck <13520622+Bomme@users.noreply.github.com>
2022-02-01 12:43:18 +0100

    remove reference to separate script

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,15 @@ import logging
 
 import streamlit as st
 
-import separate_script
-
 params = st.experimental_get_query_params()
 logging.info(f"{params=}")
 if not "my-field" in params:
   st.session_state["MY_FIELD"] = "some default value"
 
-params_separate = separate_script.get_query_params()
+params_second = st.experimental_get_query_params()
 if "MY_FIELD" in st.session_state:
-  logging.info(f"{params_separate=} {st.session_state['MY_FIELD']=}")
+  logging.info(f"{params_second=} {st.session_state['MY_FIELD']=}")
 else:
-  logging.info(f"{params_separate=}")
+  logging.info(f"{params_second=}")
 
-st.write("Main script")
 st.write(params)
-
-st.write("separate script")
-st.write(params_separate)
