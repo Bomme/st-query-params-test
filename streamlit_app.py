d6dc9496e023c47ad5cdeb057bde7310e24e3d66
Benno Weck <13520622+Bomme@users.noreply.github.com>
2022-02-01 12:45:40 +0100

    indent by 4 spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ import streamlit as st
 params = st.experimental_get_query_params()
 logging.info(f"{params=}")
 if not "my-field" in params:
-  st.session_state["MY_FIELD"] = "some default value"
+    st.session_state["MY_FIELD"] = "some default value"
 
 params_second = st.experimental_get_query_params()
 if "MY_FIELD" in st.session_state:
-  logging.info(f"{params_second=} {st.session_state['MY_FIELD']=}")
+    logging.info(f"{params_second=} {st.session_state['MY_FIELD']=}")
 else:
-  logging.info(f"{params_second=}")
+    logging.info(f"{params_second=}")
 
 st.write(params)
